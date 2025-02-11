e127c000a49deda237e2686da30c8ff21e7a8b23
Gnanendra Reddy K <gnanendra.reddyk@philips.com>
2025-02-11 20:51:16 +0530

    Unit tests project initial check in

---
+++ BasicCalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicCalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TA-CASE")]
+[assembly: AssemblyProduct("BasicCalculatorTests")]
+[assembly: AssemblyCopyright("Copyright © TA-CASE 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0c4acb5-b009-4f0f-a296-7a030c90953f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
